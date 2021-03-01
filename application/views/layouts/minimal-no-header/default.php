4e5599bb7acae6788efafaf5ba00d8e59493d4b4
Dinesh <dinesh.suthar@g-axon.com>
2021-03-01 14:21:09 +0530

    minimal-no-header remove header by default

--- application/views/layouts/minimal-no-header/default.php
+++ application/views/layouts/minimal-no-header/default.php
@@ -20,7 +20,21 @@
 
 				<!-- Site Content Wrapper -->
 				<div class="dt-content-wrapper">
-					<?php include view_path('partials/header.php'); ?>
+
+					<div class="d-flex align-items-center px-4 px-sm-10 pt-5 pt-sm-8">
+						<!-- Brand tool -->
+						<div id="dt-brand__tool" class="dt-brand__tool d-lg-none" data-toggle="main-sidebar">
+							<i class="material-icons">menu</i>
+						</div>
+						<!-- /brand tool -->
+
+						<div class="alert alert-primary alert-dismissible fade show mx-auto mb-0" role="alert">
+							A new version has been arrived, Click here to update jumbo 5.0
+							<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+								<span aria-hidden="true">&times;</span>
+							</button>
+						</div>
+					</div>
 
 					<!-- Site Content -->
 					<div class="dt-content">
